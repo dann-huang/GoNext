a41eb0d9a5f6390b196196607cad02b6ff8f109a
Daniel <me@daniel-h.ca>
2025-06-20 18:22:12 -0700

    change to pointer for touch screen

--- frontend/src/components/games/chess/ChessBoard.tsx
+++ frontend/src/components/games/chess/ChessBoard.tsx
@@ -24,7 +24,15 @@ export function ChessBoard({ gameState, makeMove }: GameBoardProps) {
   const idx = gameState.players.indexOf(username);
   const yourTurn = gameState.status === 'in_progress' && gameState.turn === idx;
 
-  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>, piece: number, row: number, col: number) => {
+  const boardRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleTouchStart = (e: TouchEvent) => e.preventDefault();
+    boardRef.current?.addEventListener('touchstart', handleTouchStart, { passive: false });
+    return () => boardRef.current?.removeEventListener('touchstart', handleTouchStart);
+  }, [boardRef.current]);
+
+  const handlePointerDown = (e: React.PointerEvent<HTMLDivElement>, piece: number, row: number, col: number) => {
+    if (e.pointerType === 'mouse' && e.button !== 0) return;
     setDrag({
       piece: piece,
       from: { row, col },
@@ -33,40 +41,37 @@ export function ChessBoard({ gameState, makeMove }: GameBoardProps) {
     e.preventDefault();
   };
 
-
   useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setDrag(prev => {
-        if (!prev) return null;
-        return {
-          ...prev,
-          pos: { x: e.clientX, y: e.clientY },
-        };
-      });
+    if (!drag) return;
+    const handlePointerMove = (e: PointerEvent) => {
+      setDrag(prev => ({
+        ...prev!,
+        pos: { x: e.clientX, y: e.clientY },
+      }));
     };
-
-    const handleMouseUp = (e: MouseEvent) => setDrag(drag => {
-      if (!drag || !hover.current) return null;
-      makeMove({
-        from: drag.from,
-        to: hover.current,
-      });
-      return null;
-    });
-
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
+    const handlePointerUp = (e: PointerEvent) => {
+      if (hover.current) {
+        makeMove({
+          from: drag.from,
+          to: hover.current,
+        });
+      }
+      setDrag(null);
+    };
+    document.addEventListener('pointermove', handlePointerMove);
+    document.addEventListener('pointerup', handlePointerUp, { once: true });
     return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('pointermove', handlePointerMove);
+      document.removeEventListener('pointerup', handlePointerUp);
     };
-  }, [setDrag]);
+  }, [drag, makeMove]);
 
   const validSquares = useMemo(() => gameState.validMoves.filter(vMove =>
     vMove.from?.row === drag?.from.row && vMove.from?.col === drag?.from.col)
     , [gameState.validMoves, drag?.from.row, drag?.from.col]);
 
-  return <div className="w-full grid grid-cols-8 border-3 border-secondary">
+  return <div className="w-full grid grid-cols-8 border-3 border-secondary" ref={boardRef}>
+    <h1 className='col-span-full text-center text-xl text-primary'>attention: work in progress</h1>
     {gameState.board.map((cellRow, row) =>
       cellRow.map((cell, col) => {
         const isLight = (row + col) % 2 === 0;
@@ -82,9 +87,9 @@ export function ChessBoard({ gameState, makeMove }: GameBoardProps) {
               ? isLight ? 'bg-primary/33' : 'bg-primary/66'
               : isLight ? 'bg-secondary/33' : 'bg-secondary/66',
           )}
-          onMouseEnter={() => hover.current = { row, col }}
-          onMouseLeave={() => hover.current = null}
-          onMouseDown={(e) => handleMouseDown(e, cell, row, col)}
+          onPointerEnter={() => hover.current = { row, col }}
+          onPointerLeave={() => hover.current = null}
+          onPointerDown={(e) => handlePointerDown(e, cell, row, col)}
         >
           {Piece
             && !(drag && drag.from.row === row && drag.from.col === col)
